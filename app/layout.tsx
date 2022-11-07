f3958f078d49f4fa257f5e0533038ee53b914c38
capJavert <ante@kickass.website>
2022-11-07 14:35:24 +0100

    feat remove Script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,8 @@
-import Script from 'next/script'
-
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <html lang="en">
             <head></head>
             <body>{children}</body>
-            {/* it does not matter which script we are loading here, imagine it is not required for app code but need as 3party eg. after interactivity */}
-            <Script strategy="afterInteractive" src="https://code.jquery.com/jquery-3.6.1.min.js" />
         </html>
     )
 }
